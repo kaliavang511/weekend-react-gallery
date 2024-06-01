85a05f871155deb7cf6b3985e774660ea5db9f2c
Kalia Vang <kaliavang511@gmail.com>
2024-05-31 23:18:40 -0500

    fixed css

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -3,12 +3,6 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 
-
-// PUT /gallery/like/:id
-router.put('/like/:id', (req, res) => {
-  // code here
-});
-
 router.get('/', (req, res) => {
   console.log("In GET request");
   let queryText = 'SELECT * from "gallery"';
@@ -20,5 +14,21 @@ router.get('/', (req, res) => {
       res.sendStatus(500);
   })
 });
+
+
+
+router.post('/', (req, res) => {
+  const liked = req.body;
+  const sqlText = `INSERT INTO food ("likes")
+                   VALUES ($1,)`;
+  pool.query(sqlText, [liked.like,])
+      .then((result) => {
+          res.sendStatus(201);
+      })
+      .catch((error) => {
+          console.log(`Error making database query ${sqlText}`, error);
+          res.sendStatus(500); // Good server always responds
+      })
+})
 
 module.exports = router;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,35 @@
 import { useState } from "react";
 import axios from 'axios'
 import { useEffect  } from "react";
+import './App.css';
 
 function App() {
   const [galleryList, setGalleryList] = useState([]);
+  let [numLikes, setNumLikes] = useState(0)
+  let [numLikes2, setNumLikes2] = useState(0)
+  let [numLikes3, setNumLikes3] = useState(0)
 
+
+  const handleClick = () => {
+    setNumLikes(numLikes + 1)
+  }
+
+  const handleClick2 = () => {
+    console.log("handleClick was executed")
+    setNumLikes2(numLikes2 + 1)
+  }
+
+  const handleClick3 = () => {
+    console.log("handleClick was executed")
+    setNumLikes3(numLikes3 + 1)
+  }
+
+
+  //put route
+
+
+
+  
 
   const fetchGalleryList = () => {
     axios({
@@ -14,40 +39,64 @@ function App() {
       .then((response) => {
         console.log('response.data is:', response.data);
 
-        // Store the creatures array in React state:
+      
         setGalleryList(response.data);
       })
       .catch((error) => {
         console.log('Error on get:', error);
       });
+
+
+      
   }
   useEffect( () => {
     fetchGalleryList();
   }, [])
-
-
-    return (
-
+  return (
     <div>
-      <header>
+       <header>
         <h1>React Gallery</h1>
       </header>
 
       <p>The gallery goes here!</p>
-      <img src="images/goat_small.jpg"/>
-      <img src="images/goat_stache.png"/>
-    
-      <p>
-      {galleryList.map((gallery)=>{
-            console.log('gallery is',gallery)
-          return(
-            <p key={gallery.id}> {gallery.title} {gallery.description}</p>
-          )
-        })}
-        </p>
       
+
+    <div className="container">
+      <div className="image-wrapper">
+        <img src="images/goat_small.jpg"/>
+        <div className="button-wrapper">
+          <button onClick={() => handleClick()}>
+            Like ❤️
+          </button>
+          <div>{numLikes} people love this</div>
+        </div>
+      </div>
+  
+      <div className="image-wrapper">
+        <img src="images/goat_stache.png"/>
+        <div className="button-wrapper">
+          <button onClick={() => handleClick2()}>
+            Like ❤️
+          </button>
+          <div>{numLikes2} people love this</div>
+        </div>
+      </div>
+  
+      <div className="image-wrapper">
+        <img src="https://media-be.chewy.com/wp-content/uploads/2021/06/01091747/Japanese-Chin-FeaturedImage-1024x615.jpg"/>
+        <div className="button-wrapper">
+          <button onClick={() => handleClick3()}>
+            Like ❤️
+          </button>
+          <div>{numLikes3} people love this</div>
+        </div>
+      </div>
     </div>
-    )  
+    </div>
+  );
+  
+
+    
 
 }
 
