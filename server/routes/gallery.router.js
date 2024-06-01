ad3822e4ffb2dfc92749fea17087d49236eb4cfb
Kalia Vang <kaliavang511@gmail.com>
2024-05-31 21:38:06 -0500

    .get works

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../modules/pool.js');
+
+
 
 // PUT /gallery/like/:id
 router.put('/like/:id', (req, res) => {
   // code here
 });
 
-// GET /gallery
 router.get('/', (req, res) => {
-  // code here
+  console.log("In GET request");
+  let queryText = 'SELECT * from "gallery"';
+
+  pool.query(queryText).then((result) => {
+      res.send(result.rows);
+  }).catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+  })
 });
 
 module.exports = router;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,56 @@
+import { useState } from "react";
+import axios from 'axios'
+import { useEffect  } from "react";
 
 function App() {
-    return (
-      <div>
-        <header>
-          <h1>React Gallery</h1>
-        </header>
+  const [galleryList, setGalleryList] = useState([]);
 
-        <p>The gallery goes here!</p>
-        <img src="images/goat_small.jpg"/>
-        <img src="images/goat_stache.png"/>
-      </div>
-    );
+
+  const fetchGalleryList = () => {
+    axios({
+      method: 'GET',
+      url: '/api/gallery'
+    })
+      .then((response) => {
+        console.log('response.data is:', response.data);
+
+        // Store the creatures array in React state:
+        setGalleryList(response.data);
+      })
+      .catch((error) => {
+        console.log('Error on get:', error);
+      });
+  }
+  useEffect( () => {
+    fetchGalleryList();
+  }, [])
+
+
+    return (
+
+    <div>
+      <header>
+        <h1>React Gallery</h1>
+      </header>
+
+      <p>The gallery goes here!</p>
+      <img src="images/goat_small.jpg"/>
+      <img src="images/goat_stache.png"/>
+    
+      <p>
+      {galleryList.map((gallery)=>{
+            console.log('gallery is',gallery)
+          return(
+            <p key={gallery.id}> {gallery.title} {gallery.description}</p>
+          )
+        })}
+        </p>
+      
+    </div>
+    )  
+
 }
 
 export default App;
+      
+
